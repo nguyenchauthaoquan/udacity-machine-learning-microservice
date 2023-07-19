4fedc684d35644755add28ffb684592491d7c05a
nguyenchauthaoquan <QuanNCT@outlook.com>
2023-07-20 00:22:07 +0700

    Implement all project steps (#2)
    * Add circle CI configurations
    
    * Resolve error update circle CI configurations
    
    * Resolve error execution circle CI
    
    * Resolve error execution circle CI
    
    * Resolve error execution circle CI
    
    * Resolve error execution circle CI
    
    * Resolve error execution circle CI
    
    * Resolve error execution circle CI
    
    * Resolve docker missing commands
    
    * Resolve docker missing commands
    
    * Update circleci config gile
    
    * Update config files
    
    * rename circle ci config files and docker config
    
    * re-implement circle ci config
    
    * Modify the version of circle ci config file
    
    * Implement kubenetes step

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=microservice-machine-learning .
 
 # Step 2: 
 # List docker images
+docker image list
 
 # Step 3: 
 # Run flask app
+docker run -p 8080:80 microservice-machine-learning
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=quannct/microservice-machine-learning
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login
+docker tag microservice-machine-learning $dockerpath:latest
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
